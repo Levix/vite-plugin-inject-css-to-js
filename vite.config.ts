b3423026fc1fae99eab18db8a8964c963a8f5c7f
Zhiwei Li <65117011+Levix@users.noreply.github.com>
2023-07-18 11:16:18 +0800

    feat: update rollupOptions.external and minify

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import typescript from '@rollup/plugin-typescript';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import { readFileSync } from 'fs';
+
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url)).toString());
 
 export default defineConfig({
     build: {
-        minify: true,
+        minify: false,
         manifest: true,
         reportCompressedSize: true,
         lib: {
@@ -14,7 +17,7 @@ export default defineConfig({
             formats: ['es', 'cjs'],
         },
         rollupOptions: {
-            external: ['vite', 'rollup'],
+            external: ['vite', 'rollup', ...Object.keys(pkg.dependencies)],
             plugins: [
                 typescriptPaths({
                     preserveExtensions: true,
